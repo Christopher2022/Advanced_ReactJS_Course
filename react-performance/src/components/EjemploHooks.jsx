18f4508f7d6b93dd5b7b8b5be5ce9ea254504ae4
Christopher <auchris95@gmail.com>
2024-03-04 16:33:28 -0500

    termino del video #1

---
+++ react-performance/src/components/EjemploHooks.jsx
@@ -0,0 +1,41 @@
+import React, {useMemo, useCallback , useState} from 'react'
+
+// useMemo --> React.memo() --> Funciones Creacionales de componentes que deben memorizar algo
+// useCallBack --> React.useCallback() Memorizar valores listados en la dependencia. Funciones en línea
+
+
+
+const MiComponente = ({names}) => {
+
+    const [name, setName]= useState('');
+    const names = [
+        'Martin',
+        'Eric'
+    ]
+
+    const getName = useCallback(
+        () => {
+            //obtenemos un nombre aleatorio
+            const random = Math.floor(Math.random()* (names.length-1));
+            setName(names[random]);
+        },
+        [name],
+    );
+
+    const clearName = useCallback(
+        () => {
+            setName(null);
+
+        }, [])
+
+    return (
+        <h1>
+            Mi Componente
+        </h1>
+    )
+}
+
+
+
+
+// const Memoization = useMemo ( () => MiComponente, [a,b])
