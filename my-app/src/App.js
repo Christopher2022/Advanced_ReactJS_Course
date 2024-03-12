f78786f19dbe3d030bfa4d0052f22a6264c48b64
Christopher <auchris95@gmail.com>
2024-03-11 21:39:41 -0500

    final del video #5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import { withServiceWorkerUpdater } from '@3m1/service-worker-updater'
 
-function App() {
+const App = (props) => {
+  const {newServiceWorkerDetected, onLoadNewServiceWorkerAccept}  = props;
+
+  const [newItem, setNewItem] = React.useState("");
+  const [items, setItem] = React.useState([]);
+
+  const handleAddItem = () => {
+    //Agregar el nuevo item a la lista de items
+    setItem([...items, newItem]);
+
+    //Limpiar valores del input
+    setNewItem('');
+  }
+
+  // const handleKeyPress = event => {
+  //   if (event.key === 'Enter') {
+  //     handleAddItem();
+  //   }
+  // };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>**Proyecto PWA - Lista de compra v8**</h1>
+        { newServiceWorkerDetected && <div style={{ backgroundColor: 'red', marginBottom: 20 }}>
+          <h3>Nueva actualización! ¿Quieres actualizar?</h3>
+          <button onClick={onLoadNewServiceWorkerAccept}>Actualizar!</button>
+        </div>}
+        <input style={{ fontSize: '22px' }} type="text" onKeyPress={e => e.key === 'Enter' && handleAddItem()} onChange={e => setNewItem(e.target.value)} value={newItem} />
+        <button style={{ fontSize: '22px' }} onClick={handleAddItem} >Añadir</button>
+        <ul>
+          {items.map((item, key) => <li key={key}>{item}</li>)}
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withServiceWorkerUpdater(App);
