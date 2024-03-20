6c323e0fcf62bc5327690d693c4acace08155edc
Christopher <auchris95@gmail.com>
2024-03-20 11:54:59 -0500

    final del video #112

--- session-testing/src/App.test.js
+++ session-testing/src/App.test.js
@@ -1,10 +1,11 @@
-import {render} from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import App from "./App";
 import ListadoNotas from "./components/ListadoNotas";
+import InputNuevaNota from "./components/InputNuevaNota";
 
 describe('React- Testeamos los componentes', () => {
     test('El listado se renderiza correctamente', () => {
-        const r = render(<ListadoNotas/>);
+        const r = render(<ListadoNotas />);
         expect(r).toBeDefined();
     })
     test('El listado se renderiza de un listado correctamente', () => {
@@ -20,15 +21,47 @@ describe('React- Testeamos los componentes', () => {
         // expect(r).toBeDefined();
         expect(div.childElementCount).toBe(2);
     })
-    
-    
+
 })
 
 describe('REACT - Hacemos un test de integración', () => {
     test('Renderizamos la app ', () => {
-      const r = render(<App/>)
-      expect(r).toBeDefined();
+        const r = render(<App />)
+        expect(r).toBeDefined();
     })
-    
+
+    test('Renderizamos el input ', () => {
+        const placeholdertext = "Introduce una nueva nota";
+        const r = render(<App />)
+        const input = r.getByPlaceholderText(placeholdertext);
+        expect(input).toBeDefined();
+    })
+
+    test('Cuando hacemos clic en el botón Añadir, se lanza el evento', () => {
+
+        const funcionMock = jest.fn();
+        const r = render(<InputNuevaNota addNuevaNota={funcionMock} />);
+        const button = r.getByText("Añadir");
+        fireEvent.click(button);
+        expect(funcionMock).toHaveBeenCalledTimes(1);
+    })
+
+    test('Añadir una nueva nota ', () => {
+        const placeholdertext = "Introduce una nueva nota";
+        const r = render(<App />)
+        const input = r.getByPlaceholderText(placeholdertext);
+        const button = r.getByText("Añadir");
+        const div = r.getByLabelText('listado-notas');
+        const hijosInicial = div.childElementCount;
+        fireEvent.change(input, { target: { value: 'Poner gasolina' } });
+        fireEvent.click(button);
+        const hijosFinal = div.childElementCount;
+        expect(hijosFinal).toBeGreaterThan(hijosInicial);
+        expect(hijosInicial).toBeLessThan(hijosFinal);
+        expect(hijosInicial).not.toBe(hijosFinal);
+    })
+
+
+
 });
 
