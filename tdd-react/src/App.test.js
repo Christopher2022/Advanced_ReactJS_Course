0e977fa35e8a10fd75c6a88336ec4d20a7c2119a
Christopher <auchris95@gmail.com>
2024-03-20 15:58:16 -0500

    Avance del video #12

---
+++ tdd-react/src/App.test.js
@@ -0,0 +1,8 @@
+//vamos a constuir una aplicacion de seleccion y busqueda de emisoras de radio en streaming
+
+// 0 - La aplicación debe renderizar correctamente
+// 1 - El nombre de la aplicación debe mostrarse en algún lugar "OpenRadioCamp"
+//2 - Debemos poder buscar radios por nombre 
+//2a- La aplicación debe tener un campo input con el placeholder => "Escribe el nombre de la radio"
+//2b- La aplicación debe tener un botón de búsqueda => Texto "Buscar"
+//2c- Cuando hacemos clic en el botón buscar, se debe ejecutar la función de búsqueda una sola vez
