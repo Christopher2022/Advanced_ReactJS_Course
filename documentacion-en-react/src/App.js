2dde51b758d8febfa4a1d3134fd61d6b3900002f
Christopher <auchris95@gmail.com>
2024-03-25 15:24:24 -0500

    Se cambio el tipo de archivo con fines del video #14

--- documentacion-en-react/src/App.js
+++ documentacion-en-react/src/App.js
@@ -1,13 +1,20 @@
-import * as React from 'react';
+import * as React from 'react'; // se importa asi para el uso de componentes y escaneo con flow
 import './App.css';
 
+const str: string = 'Hola Mundo';
 const num: number = 6;
 
 type AppProps = {
-  saludo: string
+  saludo: string,
+  numero?: number
 }
 
-function App(props: AppProps): React.Node {
+// type AppProps = {
+//   saludo: string
+// }
+
+// function App(props: AppProps): React.Node {
+function App(props: AppProps):React.Node {
   return (
     <div className="App">
       {props.saludo}
