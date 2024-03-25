eccfa1299b02b639bde68a5a43c815e78b6a921d
Christopher <auchris95@gmail.com>
2024-03-25 18:09:56 -0500

    Avance del video #15

--- react-performance-01/src/components/App.jsx
+++ react-performance-01/src/components/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import TaskList from "./lists/TaskList";
 // import Settings from "./settings/Settings";
 
+/**
+ * Función Anónima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicación
+ */
 
 
 const App = () => {
