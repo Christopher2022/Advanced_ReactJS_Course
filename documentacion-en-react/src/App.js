0502aacb67f4efa77f11a4c292ded092d42ae6e2
Christopher <auchris95@gmail.com>
2024-03-25 17:06:31 -0500

    Final del video #14

--- documentacion-en-react/src/App.js
+++ documentacion-en-react/src/App.js
@@ -13,11 +13,58 @@ type AppProps = {
 //   saludo: string
 // }
 
+/**
+ * Aplicación principal sobre la  que trabajamos
+ *
+ * @param {*} props
+ * @returns
+ */
+
 // function App(props: AppProps): React.Node {
-function App(props: AppProps):React.Node {
+function App(props: AppProps): React.Node {
+  const [darkMode, setDarkMode] = React.useState(true);
+  const [tasklist, setTaskList] = React.useState([]);
+  const [newTask, setNewTask] = React.useState("");
+
+  /**
+   *
+   * @returns Cambia el modo de light -> dark y viceversa
+   */
+  const toggleDark = () => setDarkMode(!darkMode);
+
+  /**
+   * Esta función cambia el estado newTask a través del input
+   *
+   * @param {*} e - Evento de change proveniente de react
+   * @returns
+   */
+
+  const handleChange = e => setNewTask(e.target.value);
+
+  /**
+   *
+   * @returns Añade una nueva tarea a lista de tareas
+   */
+  const addNewTask = () => setTaskList([...tasklist, newTask]);
+
+  /**
+   * 
+   * @returns true si la lista esta vacía
+   */
+  const tasklistEmpty = () => tasklist.length === 0;
+
+
   return (
-    <div className="App">
-      {props.saludo}
+    <div className={`App ${darkMode && "dark"}`}>
+      <h1>{props.saludo}</h1>
+      <button className='toggleDark' onClick={toggleDark}>Cambiar modo</button>
+      <h2>Task List</h2>
+      <div>
+        <input style={{ marginRight: 5 }} type="text" value={newTask} onChange={handleChange} />
+        <button className='toggleDark' onClick={addNewTask}  > Añadir Task</button>
+        <div>{tasklistEmpty() ? <h3>La lista de tareas está  vacía</h3> :
+          tasklist.map((t, i) => <p key={i}>{t}</p>)}</div>
+      </div>
     </div>
   );
 }
