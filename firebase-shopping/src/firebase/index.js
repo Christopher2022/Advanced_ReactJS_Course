be1357e14e8cb13e5221509a4bddd0ebd2f48ded
Christopher <auchris95@gmail.com>
2024-03-31 18:43:49 -0500

    Final del video #19

---
+++ firebase-shopping/src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAJSS3clNqkRfpegh0qxJfhFNMoIRpX_I",
+  authDomain: "fir-shopping-39d7a.firebaseapp.com",
+  projectId: "fir-shopping-39d7a",
+  storageBucket: "fir-shopping-39d7a.appspot.com",
+  messagingSenderId: "687072793132",
+  appId: "1:687072793132:web:f5e449b497c94d8cc23789"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
